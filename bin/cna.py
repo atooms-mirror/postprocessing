210ed0df5f7d1439ed16be5ed262bb4de8dd83e5
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-01-31 10:47:01 +0100

    Update factory in cna; drop olf f90wrap file

--- bin/cna.py
+++ bin/cna.py
@@ -24,6 +24,8 @@ from atooms.utils import add_first_last_skip, fractional_slice
 from pyutils.histogram import Histogram
 from postprocessing.neighbors import get_neighbors
 
+# Update factory
+Trajectory.update(__name__)
 
 def cna(particle, neighbors):
     # Add neighbor list to particles as sets
@@ -51,80 +53,84 @@ def cna(particle, neighbors):
             data.append('%d_%d_%d' % (1,len(common),bonds))
     return data
 
-parser = argparse.ArgumentParser()
-parser = add_first_last_skip(parser, what=['first', 'last'])
-parser.add_argument('-n',              dest='neigh_file', help='neighbors file')
-parser.add_argument('-N', '--neighbor',dest='neigh', type=str, default='', help='flags for neigh.x command')
-parser.add_argument('-V', '--neighbor-voronoi',dest='neigh_voronoi', action='store_true', help='neigh_file is of Voronoi type')
-parser.add_argument('-M', '--neighbor-max',dest='neigh_limit', type=int, default=None, help='take up to *limit* neighbors (assuming they are ordered)')
-parser.add_argument(      '--rcut', dest='rcut', help='cutoff radii as comma separated string, ex r11,r12,r22')
-parser.add_argument('-s', '--signature',dest='signature', help='signature')
-parser.add_argument('-o', '--output',dest='output', action='store_true', help='write to file')
-parser.add_argument('-t', '--tag',     dest='tag', type=str, default='', help='tag to add before suffix')
-parser.add_argument(nargs='+', dest='files',type=str, help='input files')
-args = parser.parse_args()
+def main(args):
+    if len(args.tag) > 0:
+        args.tag = '_' + args.tag
 
-if len(args.tag) > 0:
-    args.tag = '_' + args.tag
-
-# Unpack cut off radii as numpy array
-# TODO: refactor, this is in common with neigh.py
-if args.neigh_file is None:
-    desc = 'rcut:' + args.rcut
-    rc = args.rcut.split(',')
-    # This is the number of species given the number of independent cut off radii
-    nsp = (-1 + int(numpy.ceil((1+8*len(rc))**0.5))) / 2
-    args.rcut = numpy.ndarray((nsp, nsp))
-    i = 0
-    for isp in range(nsp):
-        for jsp in range(isp,nsp):
-            args.rcut[isp, jsp] = float(rc[i])
-            i+=1
-else:
-    desc = 'neighbors_file:' + args.neigh_file
-
-# Handle multiple signatures: make a list of them
-if args.signature is not None:
-    args.signature = args.signature.split(',')
-
-for finp in args.files:
-    t = Trajectory(finp)
-    tn = get_neighbors(finp, args, os.path.basename(sys.argv[0]))
-
-    # Fraction of selected CNA bonds (signature argument)
-    if args.signature is not None:
-        fh = dict()
-        for sign in args.signature:
-            fout = finp + '.cna%s.fraction-%s' % (args.tag, sign)
-            fh[sign] = open(fout, 'w', buffering=0)
-            fh[sign].write('# Fraction of CNA bond %s; neighbors: %s\n' % (sign, desc))
-
-    # Loop over samples
-    hist = defaultdict(int)
-    for i, s in enumerate(t):
-        if t.steps[i] in tn.steps:
-            ii = tn.steps.index(t.steps[i])
-            data = cna(t[i].particle, tn[ii].neighbors)
-            # Dump signature
-            if args.signature is not None:
-                for sign in args.signature:
-                    x = len([d for d in data if d == sign]) / float(len(data))
-                    fh[sign].write('%d %s\n' % (t.steps[i], x))
-            # Fill histogram
-            for d in data:
-                hist[d]+=1
-
-    # Write histogram
-    with open(finp + '.cna%s.hist' % args.tag, 'w') as fhhist:
-        norm = sum(hist.values())
-        fhhist.write('# CNA bonds histogram; %s\n' % desc)
-        for d in sorted(hist, key=hist.get, reverse=True):
-            fhhist.write('%s %g\n' % (d, hist[d] / float(norm)))
-
-    if args.signature is not None:
-        for fhi in fh.values():
-            fhi.close()
-
+    # Unpack cut off radii as numpy array
+    # TODO: refactor, this is in common with neigh.py
     if args.neigh_file is None:
-        os.remove(tn.filename)
+        desc = 'rcut:' + args.rcut
+        rc = args.rcut.split(',')
+        # This is the number of species given the number of independent cut off radii
+        nsp = (-1 + int(numpy.ceil((1+8*len(rc))**0.5))) / 2
+        args.rcut = numpy.ndarray((nsp, nsp))
+        i = 0
+        for isp in range(nsp):
+            for jsp in range(isp,nsp):
+                args.rcut[isp, jsp] = float(rc[i])
+                i+=1
+    else:
+        desc = 'neighbors_file:' + args.neigh_file
 
+    # Handle multiple signatures: make a list of them
+    if args.signature is not None:
+        args.signature = args.signature.split(',')
+
+    for finp in args.files:
+        t = Trajectory(finp)
+        tn = get_neighbors(finp, args, os.path.basename(sys.argv[0]))
+
+        # Fraction of selected CNA bonds (signature argument)
+        if args.signature is not None:
+            fh = dict()
+            for sign in args.signature:
+                fout = finp + '.cna%s.fraction-%s' % (args.tag, sign)
+                fh[sign] = open(fout, 'w', buffering=0)
+                fh[sign].write('# Fraction of CNA bond %s; neighbors: %s\n' % (sign, desc))
+
+        # Loop over samples
+        hist = defaultdict(int)
+        for i, s in enumerate(t):
+            if t.steps[i] in tn.steps:
+                ii = tn.steps.index(t.steps[i])
+                data = cna(t[i].particle, tn[ii].neighbors)
+                # Dump signature
+                if args.signature is not None:
+                    for sign in args.signature:
+                        x = len([d for d in data if d == sign]) / float(len(data))
+                        fh[sign].write('%d %s\n' % (t.steps[i], x))
+                # Fill histogram
+                for d in data:
+                    hist[d]+=1
+
+        # Write histogram
+        with open(finp + '.cna%s.hist' % args.tag, 'w') as fhhist:
+            norm = sum(hist.values())
+            fhhist.write('# CNA bonds histogram; %s\n' % desc)
+            for d in sorted(hist, key=hist.get, reverse=True):
+                fhhist.write('%s %g\n' % (d, hist[d] / float(norm)))
+
+        if args.signature is not None:
+            for fhi in fh.values():
+                fhi.close()
+
+        if args.neigh_file is None:
+            os.remove(tn.filename)
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser = add_first_last_skip(parser, what=['first', 'last'])
+    parser.add_argument('-n',              dest='neigh_file', help='neighbors file')
+    parser.add_argument('-N', '--neighbor',dest='neigh', type=str, default='', help='flags for neigh.x command')
+    parser.add_argument('-V', '--neighbor-voronoi',dest='neigh_voronoi', action='store_true', help='neigh_file is of Voronoi type')
+    parser.add_argument('-M', '--neighbor-max',dest='neigh_limit', type=int, default=None, help='take up to *limit* neighbors (assuming they are ordered)')
+    parser.add_argument(      '--rcut', dest='rcut', help='cutoff radii as comma separated string, ex r11,r12,r22')
+    parser.add_argument('-s', '--signature',dest='signature', help='signature')
+    parser.add_argument('-o', '--output',dest='output', action='store_true', help='write to file')
+    parser.add_argument('-t', '--tag',     dest='tag', type=str, default='', help='tag to add before suffix')
+    parser.add_argument(nargs='+', dest='files',type=str, help='input files')
+    args = parser.parse_args()
+
+    main(args)
