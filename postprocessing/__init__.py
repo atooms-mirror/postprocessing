7405c033a3393940ae22749ae5f0e10b5fa32819
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-03-10 18:14:40 +0100

    Bring logging back

--- postprocessing/__init__.py
+++ postprocessing/__init__.py
@@ -1,6 +1,9 @@
 # This file is part of atooms
 # Copyright 2010-2014, Daniele Coslovich
 
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 from .partial import *
 from .realspace import *
 from .fourierspace import *
