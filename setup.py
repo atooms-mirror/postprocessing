e59afe6834d359b93c7cc09750bc563744b0e7fe
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2017-10-15 00:03:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,11 @@ import glob
 # We use numpy distutils to compile and wrap f90 code via f2py
 from numpy.distutils.core import setup, Extension
 
+with open('postprocessing/_version.py') as f:
+    exec(f.read())
+
 args = dict(name='postprocessing',
-            version='0.1',
+            version=__version__,
             description='Post processing tools for molecular simulations',
             author='Daniele Coslovich',
             author_email='daniele.coslovich@umontpellier.fr',
@@ -16,7 +19,15 @@ args = dict(name='postprocessing',
             scripts=glob.glob(os.path.join('bin', '*.py')),
             install_requires=['atooms>=1', 'numpy', 'scipy', 'argh'],
             ext_modules=[Extension('postprocessing.neighbors_wrap', 
-                                   sources=['postprocessing/neighbors.f90'])]
+                                   sources=['postprocessing/neighbors.f90'])],
+            license='GPLv3',
+            classifiers=[
+                'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                'Development Status :: 5 - Production/Stable',
+                'Intended Audience :: Science/Research',
+                'Programming Language :: Python :: 2',
+                'Topic :: Scientific/Engineering :: Physics',
+            ]
 )
 
 setup(**args)
