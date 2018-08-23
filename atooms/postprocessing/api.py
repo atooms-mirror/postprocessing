2e6e773fab76cc65ecf1f2dac3817114d1b0e6d7
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2018-08-23 23:45:06 +0200

    Delete dangling test function that called f90 code

--- atooms/postprocessing/api.py
+++ atooms/postprocessing/api.py
@@ -9,23 +9,6 @@ from atooms.system.particle import distinct_species
 from .helpers import linear_grid, logx_grid
 
 
-def test_sk():
-    import numpy
-    import postprocessing.fourierspace_wrap
-    #from postprocessing.fourierspace_wrap import fourierspace_module import sk_one
-    from postprocessing.fourierspace_wrap import fourierspace_module
-    nk = 10
-    ndim, N = 3, 10
-    pos = numpy.zeros((ndim, N))
-    k0 = 1.0
-    ikvec = numpy.ones((ndim, nk), dtype=numpy.int32)
-    ikbin = numpy.ones(nk, dtype=numpy.int32)
-    #sk = numpy.zeros(max(ikbin), dtype=numpy.complex64)
-    sk = numpy.zeros(10)  #max(ikbin))
-    kmax = 10
-    fourierspace_module.sk_one(pos, k0, kmax, ikvec, ikbin, sk)
-    print sk
-
 def _get_trajectories(input_files, args):
     from atooms.trajectory import Sliced
     from atooms.core.utils import fractional_slice
