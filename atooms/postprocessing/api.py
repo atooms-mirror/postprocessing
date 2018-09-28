7403922a4cd54d769ecd3ab5e14cf7ffd43627f1
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2018-09-28 22:16:39 +0200

    Fix passing gr some options to partial functions via api

--- atooms/postprocessing/api.py
+++ atooms/postprocessing/api.py
@@ -43,10 +43,10 @@ def gr(input_file, dr=0.04, grandcanonical=False, fmt=None, species_layout=None,
     global_args = _compat(global_args, fmt=fmt, species_layout=species_layout)
     for th in _get_trajectories([input_file] + list(input_files), global_args):
         th._grandcanonical = grandcanonical
-        postprocessing.RadialDistributionFunction(th, norigins=norigins).do()
+        postprocessing.RadialDistributionFunction(th, dr=dr, norigins=norigins).do()
         ids = distinct_species(th[-1].particle)
         if len(ids) > 1:
-            Partial(postprocessing.RadialDistributionFunction, ids, th).do()
+            Partial(postprocessing.RadialDistributionFunction, ids, th, dr=dr, norigins=norigins).do()
 
 def sk(input_file, nk=20, dk=0.1, kmin=-1.0, kmax=15.0, ksamples=30, species_layout=None,
        norigins=-1, grandcanonical=False, fmt=None,
