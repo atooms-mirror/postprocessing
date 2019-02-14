707b51fcd71169c7789e24a0e915047bf1ef2c06
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2019-02-14 16:39:57 +0100

    Enforce symmetry in partial correlations

--- atooms/postprocessing/partial.py
+++ atooms/postprocessing/partial.py
@@ -47,6 +47,14 @@ class Partial(object):
                     self.partial[(isp, jsp)].tag = '%s-%s' % (isp, jsp)
                     self.partial[(isp, jsp)].tag_description = 'species pair %s-%s' % (isp, jsp)
 
+            # Enforce symmetry
+            for i in range(len(self.species)):
+                for j in range(len(self.species)):
+                    if j < i:
+                        isp = self.species[i]
+                        jsp = self.species[j]
+                        self.partial[(isp, jsp)] = self.partial[(jsp, isp)]
+
     def need_update(self):
         need = False
         for partial in self.partial.values():
