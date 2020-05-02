b3cc8630692f0e1c31f044021710319dd451d1cb
Daniele Coslovich <daniele.coslovich@umontpellier.fr>
2020-05-02 10:07:09 +0200

    Add neighbors routines to f90
    We should clean this up

--- atooms/postprocessing/realspace.f90
+++ atooms/postprocessing/realspace.f90
@@ -469,5 +469,89 @@ contains
        end do
     end do
   end subroutine distances_distinct
+
+  !! Find nearest neighbors 
+  !!
+  !! Neighbors list is in C-order for easier handling in python.
+  !! Also we give up III law because this avoids sorting the neighbors
+  !! indices later and overall this is more efficient
+  !! 
+  subroutine neighbors_full(offset,box,pos,ids,rcut,nn,neigh)
+    character(1), intent(in)  :: offset
+    real(8), intent(in)  :: box(:)
+    real(8), intent(in)  :: pos(:,:)
+    real(8), intent(in)  :: rcut(:,:)
+    integer, intent(in)  :: ids(:)
+    integer, intent(inout) :: nn(:)
+    integer, intent(inout) :: neigh(:,:)
+    real(8)              :: rij(size(pos,1)), rijsq, hbox(size(pos,1))
+    real(8)              :: rcutsq(size(rcut,1),size(rcut,2))
+    integer              :: i, j, isp, jsp, delta
+    if (offset == 'C') then
+       delta = -1
+    else
+       delta = 0
+    end if    
+    nn = 0
+    hbox = box / 2
+    rcutsq = rcut**2
+    do i = 1,size(pos,2)
+       isp = ids(i)
+       do j = i+1,size(pos,2)
+          jsp = ids(j)
+          rij = pos(:,i) - pos(:,j)
+          where (abs(rij) > hbox)
+             rij = rij - sign(box,rij)
+          end where
+          rijsq = dot_product(rij,rij)
+          if (rijsq < rcutsq(isp,jsp)) then
+             nn(i) = nn(i) + 1
+             nn(j) = nn(j) + 1
+             neigh(i,nn(i)) = j+delta
+             neigh(j,nn(j)) = i+delta
+          end if
+       end do
+    end do
+  end subroutine neighbors_full
+
+  ! Find neighbors based on a pre-existing subset of indices (typically from linked cells)
+  subroutine neighbors_subset(offset,box,pos,subset,subset_number,ids,rcut,nn,neigh)
+    character(1), intent(in)  :: offset
+    real(8), intent(in)  :: box(:)
+    real(8), intent(in)  :: pos(:,:)
+    real(8), intent(in)  :: rcut(:,:)
+    integer, intent(in)  :: ids(:),subset_number(:),subset(:,:)
+    integer, intent(inout) :: nn(:)
+    integer, intent(inout) :: neigh(:,:)
+    real(8)              :: rij(size(pos,1)), rijsq, hbox(size(box))
+    real(8)              :: rcutsq(size(rcut,1),size(rcut,2))
+    integer              :: i, j, isp, jsp, delta, jj
+    if (offset == 'C') then
+       delta = 1
+    else
+       delta = 0
+    end if    
+    nn = 0
+    hbox = box / 2
+    rcutsq = rcut**2
+    do i = 1,size(pos,2)
+       isp = ids(i)
+       do jj = 1,subset_number(i)
+          j = subset(i,jj) + delta
+          jsp = ids(j)
+          rij = pos(:,i) - pos(:,j)
+          where (abs(rij) > hbox)
+             rij = rij - sign(box,rij)
+          end where
+          rijsq = dot_product(rij,rij)
+          if (rijsq < rcutsq(isp,jsp)) then
+             nn(i) = nn(i) + 1
+             nn(j) = nn(j) + 1
+             neigh(i,nn(i)) = j-delta
+             neigh(j,nn(j)) = i-delta
+          end if
+       end do
+    end do
+  end subroutine neighbors_subset
   
 end module compute
