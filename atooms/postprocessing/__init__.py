a2f757a4e4f8e90e60a5d18d78b26f9ff9b60f5c
Daniele Coslovich <92140-coslo@users.noreply.framagit.org>
2021-10-21 17:18:33 +0200

    Trivia

--- atooms/postprocessing/__init__.py
+++ atooms/postprocessing/__init__.py
@@ -1,5 +1,8 @@
-# This file is part of atooms
-# Copyright 2010-2018, Daniele Coslovich
+"""
+Post-processing tools to compute static and dynamic correlation
+functions from simulations of interacting particles, such as molecular
+dynamics or Monte Carlo simulations.
+"""
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
